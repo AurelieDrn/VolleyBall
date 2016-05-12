9659156045afac835338fd0374955bfca66a980d
Meriem EL QSIMI <meriem.el-qsimi@etu.univ-nantes.fr>
2016-05-12 21:59:33 +0200

    Deleting tst.java via Eclipse

--- volleyball/src/tst.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public class tst {
-
-	public static void main(String[] args) {
-		System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
-	}
-
-}
