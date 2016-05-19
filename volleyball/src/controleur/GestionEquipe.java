81a502cbf9d35da4fb78103ca143ff3840effcec
Meriem EL QSIMI <meriem.el-qsimi@etu.univ-nantes.fr>
2016-05-19 17:00:36 +0200

    Màj controleur ( gestionMatch et gestionEquipe)

--- volleyball/src/controleur/GestionEquipe.java
+++ volleyball/src/controleur/GestionEquipe.java
@@ -3,10 +3,54 @@
  */
 package controleur;
 
+import java.util.List;
+
+import modele.Equipe;
+import modele.Joueur;
+import modele.Sponsor;
+
 /**
  * @author Aurelie
  *
  */
 public class GestionEquipe {
-
+	private Equipe equipe;
+	
+	public GestionEquipe(Equipe equipe) {
+		super();
+		this.equipe = equipe;
+	}
+	
+	public void recruterJoueur( Joueur joueur){
+		if(!this.equipe.equipeComplete()){
+			this.equipe.addJoueur(joueur);
+		}
+	}
+	
+	public void licencierJoueur( Joueur joueur){
+		this.equipe.deleteJoueur(joueur);
+	}
+	
+	public void reposerJoueur(Joueur joueur){
+		
+	}
+	
+	public void payerSalaireJoueurs(){
+		for(Joueur joueur : this.equipe.getMembres()){
+			joueur.setGain(joueur.getGain()+ joueur.getSalaireHebdo());
+		}
+	}
+	
+	public void obtenirSalaireEquipe(){
+		
+	}
+	
+	public void obtenirSponsor(Sponsor sponsor){
+		this.equipe.addSponsor(sponsor);
+		obtenirArgentSponsors(sponsor);
+	}
+	
+	public void obtenirArgentSponsors(Sponsor sponsor){
+		this.equipe.setBudget(this.equipe.getBudget() + sponsor.getSub());
+	}
 }
--- volleyball/src/controleur/GestionMatch.java
+++ volleyball/src/controleur/GestionMatch.java
@@ -3,10 +3,41 @@
  */
 package controleur;
 
+import modele.Match;
+
 /**
  * @author Aurelie
  *
  */
 public class GestionMatch {
+	private Match match;
 
+	public GestionMatch(Match match) {
+		super();
+		this.match = match;
+	}
+	
+	public void fatigueJoueurs(){
+		
+	}
+	
+	public void reposJoueurs(){
+		
+	}
+	
+	public void mettreAJourMatch(Match match){
+		
+	}
+	
+	public void gestionAccident(){
+		
+	}
+	
+	public void tempsMort(){
+		
+	}
+	
+	public void jouer(){
+		
+	}
 }
