95923fb0ee4cd98740606802032277337182dc02
Meriem EL QSIMI <meriem.el-qsimi@etu.univ-nantes.fr>
2016-05-19 17:26:44 +0200

    Màj, problème de sync du projet

--- volleyball/src/modele/Equipe.java
+++ volleyball/src/modele/Equipe.java
@@ -10,100 +10,59 @@ import java.util.List;
  * @author Yumiao Fu
  * relecteurs Aurélie Durand
  */
-<<<<<<< HEAD
 public class Equipe implements Comparable<Equipe>{
-	private String nom;
-	private List<Joueur> membres;
-	private int budget;
-	private List<Sponsor> sponsors;
-	private int salaireHebdo;
-	
-	public Equipe() { 
-		super();
-		this.membres = new ArrayList<Joueur>();
-		this.sponsors = new ArrayList<Sponsor>();
-		this.budget = 0;
-		this.salaireHebdo = 5000; //je sais pas comment on va l'attribuer
-		//le salaire hebdo sera-il le même pour toutes les équipes? ou bien on l'attribue lors de la création?
-	}
-	
-	public List<Joueur> getMembres() {
-		return membres;
-=======
-public class Equipe {
+
 	private int numeroEquipe;
 	private String nomEquipe;
 	private int budget;
 	private List<Joueur> listJoueurs;
 	private List<Sponsor> listSponsors;
-		
-	public Equipe(int numero, String nom, int budget){
+	private int salaireHebdo;
+	
+	public Equipe(int numero, String nom) { 
+		super();
 		this.numeroEquipe = numero;
 		this.nomEquipe = nom;
 		this.budget = budget;
 		this.listJoueurs = new ArrayList<>();
 		this.listSponsors = new ArrayList<>();
->>>>>>> branch 'master' of https://gitlab.univ-nantes.fr/MeriemQs/VolleyballJava.git
+		this.salaireHebdo = 5000; //je sais pas comment on va l'attribuer
+		//le salaire hebdo sera-il le même pour toutes les équipes? ou bien on l'attribue lors de la création?
 	}
+	
 
-<<<<<<< HEAD
-	public void setMembres(List<Joueur> membres) {
-		this.membres = membres;
-	}
+		
 
 
 	
 	public boolean equipeComplete(){
-		return this.membres.size() == 12;
+		return this.listJoueurs.size() == 12;
 	}
 	
 	public boolean addJoueur(Joueur joueur){
-		return this.membres.add(joueur);
+		return this.listJoueurs.add(joueur);
 	}
 	
 	public void deleteJoueur(Joueur joueur){
-		this.membres.remove(joueur);
+		this.listJoueurs.remove(joueur);
 	}
 	
 	public boolean addSponsor(Sponsor sponsor){
-		return this.sponsors.add(sponsor);
+		return this.listSponsors.add(sponsor);
 	}
 	
 	public void deleteJoueur(Sponsor sponsor){
-		this.sponsors.remove(sponsor);
+		this.listJoueurs.remove(sponsor);
 	}
 
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public int getBudget() {
-		return budget;
-	}
-
-	public void setBudget(int budget) {
-		this.budget = budget;
-	}
-
-	public List<Sponsor> getSponsors() {
-		return sponsors;
-	}
-
-	public void setSponsors(List<Sponsor> sponsors) {
-		this.sponsors = sponsors;
-	}
-
+	
+	
 	@Override
 	public int compareTo(Equipe eq) {
-		int last = this.nom.compareTo(eq.nom);	
-		return last == 0 ? this.nom.compareTo(eq.nom) : last;
+		int last = this.nomEquipe.compareTo(eq.nomEquipe);	
+		return last == 0 ? this.nomEquipe.compareTo(eq.nomEquipe) : last;
 	}
 	
-=======
 	public int getNumeroEquipe() {
 		return this.numeroEquipe;
 	}
@@ -120,14 +79,6 @@ public class Equipe {
 		this.nomEquipe = nomEquipe;
 	}
 
-	public int getBudget() {
-		return this.budget;
-	}
-
-	public void setBudget(int budget) {
-		this.budget = budget;
-	}
-
 	public List<Joueur> getListJoueur() {
 		return this.listJoueurs;
 	}
@@ -142,5 +93,4 @@ public class Equipe {
 	public void setListSponsors(List<Sponsor> listSponsors) {
 		this.listSponsors = listSponsors;
 	}
->>>>>>> branch 'master' of https://gitlab.univ-nantes.fr/MeriemQs/VolleyballJava.git
 }
