5af87cffeb0833f33e4aef39a1f43bc2274338f9
Aurelie <aurelie.durand1@etu.univ-nantes.fr>
2016-06-03 17:40:43 +0200

    nouveau package game

---
+++ volleyball/src/modele/JoueurBlesseException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package modele;
+
+/**
+ * @author Aurelie
+ *
+ */
+public class JoueurBlesseException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4358747003722142897L;
+
+	public String getMessage() {
+		return "Le joueur est blessé, il ne peut pas jouer ! Vous devez d'abord le soigner.";
+	}
+
+}
